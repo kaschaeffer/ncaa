5e1a2d334a5f71d43bde6db9fa23a3caf74593e6
Kevin Schaeffer <kevin.a.schaeffer@gmail.com>
2016-07-30 22:09:29 -0700

    Added scaladocs to splitters module

--- src/main/scala/ncaa/splitters/RandomRowSplitter.scala
+++ src/main/scala/ncaa/splitters/RandomRowSplitter.scala
@@ -7,12 +7,17 @@ object RandomRowSplitter {
     new RandomRowSplitter(trainingFraction)
 }
 
+/** Randomly assigns each row independently to training or test dataset.
+  *
+  * @param trainingFraction probability that a given row should be placed
+  * in the training dataset.  Must be in range [0.0, 1.0].
+  */
 class RandomRowSplitter(trainingFraction: Double) extends Splitter {
   require(
     trainingFraction >= 0.0 && trainingFraction <= 1.0,
     "trainingFraction must in range [0.0, 1.0]")
 
-  def split(df: DataFrame): SplitData = {
+  override def split(df: DataFrame): SplitData = {
     val trainingAndTest = df.randomSplit(Array(trainingFraction, 1 - trainingFraction))
     SplitData(trainingAndTest(0), trainingAndTest(1))
   }
--- src/main/scala/ncaa/splitters/Splitter.scala
+++ src/main/scala/ncaa/splitters/Splitter.scala
@@ -2,8 +2,19 @@ package ncaa.splitters
 
 import org.apache.spark.sql.DataFrame
 
+/** Disjoint data split into training and test DataFrames.
+  *
+  * @param training training data
+  * @param test     test data
+  */
 case class SplitData(training: DataFrame, test: DataFrame)
 
+/** Splits data into two disjoint sets.
+  *
+  * The split may either be deterministic or random, but each
+  * row of the original dataset must appear exactly once
+  * in either the test or training datasets.
+  */
 trait Splitter {
   def split(df: DataFrame): SplitData
 }
